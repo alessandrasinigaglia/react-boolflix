8ffef1db42066b3442abdd0319cc4ad334c77666
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-02-17 14:57:16 +0100

    milestone 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,10 @@ export default function Header() {
 
     return (
         <header>
-            <img src="/img/fontbolt.png"></img>
-            <h1>boolflix</h1>
+            <img src="/img/fontbolt.png" className="logo"></img>
+            <div className="searchbar">
             <SearchBar />
+            </div>
         </header>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,36 +1,40 @@
 import { useAppDataContext } from "../context/AppDataContext";
 
+const getFlagUrl = (languageCode) => {
+    const url = `https://purecatamphetamine.github.io/country-flag-icons/3x2/${languageCode.toUpperCase()}.svg`;
+    return url || 'https://purecatamphetamine.github.io/country-flag-icons/3x2/UN.svg';
+};
+
+const getImageUrl = (path, size = 'w342') => {
+    return `https://image.tmdb.org/t/p/${size}${path}`;
+};
+
 export default function Main() {
     const { movies } = useAppDataContext();
 
-    const getFlagUrl = (languageCode) => {
-        return `https://purecatamphetamine.github.io/country-flag-icons/3x2/${languageCode.toUpperCase()}.svg`;
-    };
-
     return (
         <main>
-            <h2>Lista Films</h2>
+        <h2>Lista Films</h2>
             <ul>
-                {
-                    movies.length > 0 ? (
-                        movies.map((movie, index) => (
-                            <li key={index}>
-                            <h3>{movie.title}</h3> 
-                            <p><strong>Titolo Originale:</strong> {movie.original_title}</p>
-                            
-                            <p><strong>Lingua:</strong> 
-                                <img src={getFlagUrl(movie.original_language)} />
-                                {movie.original_language}
-                            </p>  
-                            
-                            <p><strong>Voto:</strong> {movie.vote_average}</p>
-                        </li>
-                        ))
-                    ) : (
-                        <li>Nessun risultato trovato.</li>
-                    )
-                }
+            {
+            movies.length > 0 ? (
+            movies.map((movie, index) => (
+            <li key={index}>
+            <img src={getImageUrl(movie.poster_path)} className="movie-poster"/>
+                <h3>{movie.title}</h3> 
+                    <p><strong>Titolo Originale:</strong> {movie.original_title}</p>
+                    <p><strong>Lingua:</strong> 
+                        <img src={getFlagUrl(movie.original_language)} className="flag-icon"/>
+                            {movie.original_language}</p>  
+                    <p><strong>Voto:</strong> {movie.vote_average}</p>
+            </li>
+          ))
+          ) : (
+            <li>Nessun risultato trovato.</li>
+            )
+            }
             </ul>
         </main>
     );
 }
+
